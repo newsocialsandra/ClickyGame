ecca83febacb94efa90df3d538e6250d55832541
newsocialsandra <sandra.e.b.svensson@gmail.com>
2026-04-15 16:55:14 +0200

    Create countdown timer

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-// Click a button or object to earn points so that I can increase my score.
-// See my current score during the game so that I know how well I am doing.
-// See a countdown timer so that I know how much time is left.
 // Have the game to end automatically when the time is over (60s) so that the rules are clear and fair.
 // See my final score when the game ends so that I know my result.
 // Enter my name so that my result can be connected to me
-// Submit my score so that it can be saved to the shared scoreboard. setInterval()
+// Submit my score so that it can be saved to the shared scoreboard.
 // Get a message that tells me if my score was saved successfully or not so that I understand what happened.
 
 // Variables
 let score = 0;
 
+let time = 60;
+
 // HTML DOM
 const button1 = document.getElementById('btn1');
 const scoreDisplay = document.getElementById('scoreDisplay');
+const timeDisplay = document.getElementById('timeDisplay');
+
 
 // UI Functions
 button1.addEventListener('click', () => {
@@ -25,3 +26,8 @@ function increaseScore() {
   console.log(score);
   scoreDisplay.innerText = score;
 }
+
+myInterval = setInterval(function () {
+  timeDisplay.innerHTML = time -= 1;
+}, 1000);
+
