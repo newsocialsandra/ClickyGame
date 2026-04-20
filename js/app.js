20f58eae5bd893e923dceecbcc91d81e76ac40d7
newsocialsandra <sandra.e.b.svensson@gmail.com>
2026-04-20 09:25:31 +0200

    Add end game functionality to send in name and score

--- js/app.js
+++ js/app.js
@@ -5,14 +5,18 @@
 
 // Variables
 let score = 0;
-let time = 60;
+let time = 5;
 let gameStarted = false;
 let gameEnded = false;
+let interval = null;
 
 // HTML DOM
 const button1 = document.getElementById('btn1');
+const button2 = document.getElementById('btn2');
 const scoreDisplay = document.getElementById('scoreDisplay');
 const timeDisplay = document.getElementById('timeDisplay');
+const label1 = document.getElementById('label1');
+const input1 = document.getElementById('name');
 
 // UI Functions
 button1.addEventListener('click', () => {
@@ -22,10 +26,18 @@ button1.addEventListener('click', () => {
   }
 })
 
+button2.addEventListener('click', () => {
+  submitHighScore();
+})
+
+input1.style.display = 'none';
+label1.style.display = 'none';
+button2.style.display = 'none';
+
+
 // Functions
 function increaseScore() {
   score++;
-  console.log(score);
   scoreDisplay.innerText = score;
 }
 
@@ -45,6 +57,21 @@ function startGame() {
 }
 
 function endGame() {
-  console.log("Game ended");
   gameEnded = true;
+  clearInterval(interval);
+  button1.style.display = 'none';
+  input1.style.display = 'block';
+  label1.style.display = 'block';
+  button2.style.display = 'block';
 }
+
+function submitHighScore() {
+  console.log(input1.value);
+  // POST value to API from Ben
+}
+
+//TODO
+// Start game button text before first click
+// Show final score as Final Score: in the end
+// When game ends - show text field for user name and submit score button
+// After submitted score, show whether the score has been saved or not
